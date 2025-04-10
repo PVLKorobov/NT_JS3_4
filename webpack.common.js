7e419ac4053e60bc3d942987e00b5cea511c2be5
Pavel Korobov <korobovpvl@gmail.com>
2025-04-10 13:22:08 +0300

    icons

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   target: "web",
@@ -43,6 +44,11 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "[name].css",
       chunkFilename: "[id].css",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {from: 'licenses.txt', to: 'licenses.txt'},
+      ],
     }),
   ],
 };
