2e3cb9e627e43cd3cb555b2c2ba492ffe96684b9
Pavel Korobov <korobovpvl@gmail.com>
2025-04-12 02:35:23 +0300

    CI enviroment check and arguments

--- e2e/app.test.js
+++ e2e/app.test.js
@@ -1,109 +1,108 @@
 import puppeteer from "puppeteer";
-import { fork } from 'child_process';
-
+import { fork } from "child_process";
 
 describe("E2E Widget test", () => {
-    let browser;
-    let page;
-    let server;
-    let timeout = 30000;
-    
-    beforeAll(async () => {
-        server = fork('./e2e/test-server.js');
-        await new Promise((resolve, reject) => {
+  let browser;
+  let page;
+  let server;
+  let timeout = 30000;
 
-            server.on('error', (err) => {
-                reject(err);
-            });
+  beforeAll(async () => {
+    server = fork("./e2e/test-server.js");
+    await new Promise((resolve, reject) => {
+      server.on("error", (err) => {
+        reject(err);
+      });
 
-            server.on('message', (message) => {
-                if (message == 'ok') {
-                    resolve();
-                }
-            });
-        });
-
-        console.log('Launching the browser...');
-        browser = await puppeteer.launch({
-            // headless: false,
-            // slowMo: 50,
-            // devtools: true,
-        });
-        console.log('Launched browser');
-    }, timeout);
-    
-    afterAll(async () => {
-        await browser.close();
-        server.kill();
+      server.on("message", (message) => {
+        if (message == "ok") {
+          resolve();
+        }
+      });
     });
 
-    beforeEach(async () => {
-        if (page) {
-            await page.close();
-        }
-    })
-    
-    test.each([
-        "253422525",
-        "000001342",
-        "ger354e",
-        " 235 sdg- 23",
-        "235678976235978523",
-        "",
-    ])(
-        "Invalid card numbers are rejected",
-        async (cardNum) => {
-            page = await browser.newPage();
-            await page.goto("http://localhost:9000", {
-                timeout: timeout,
-                waitUntil: "domcontentloaded",
-            });
-            
-            const formWrapper = await page.$(
-                ".contents__wrapper .widget__wrapper .form__wrapper",
-            );
-            const form = await formWrapper.$("form");
-            const inputField = await form.$(".form__input");
-            const submitButton = await form.$(".form__submit");
-            const tooltip = await formWrapper.$(".form__tooltip");
-            
-            await inputField.type(cardNum);
-            await submitButton.click();
-            
-            expect(tooltip.innerHTML).not.toBe("");
-        },
-        timeout,
-    );
-    
-    test.each([
-        "371449635398431",
-        "30569309025904",
-        "6011111111111117",
-        "3530111333300000",
-        "5555555555554444",
-        "4111111111111111",
-    ])(
-        "Valid card numbers pass",
-        async (cardNum) => {
-            page = await browser.newPage();
-            await page.goto("http://localhost:9000", {
-                timeout: timeout,
-                waitUntil: "domcontentloaded",
-            });
-            
-            const formWrapper = await page.$(
-                ".contents__wrapper .widget__wrapper .form__wrapper",
-            );
-            const form = await formWrapper.$("form");
-            const inputField = await form.$(".form__input");
-            const submitButton = await form.$(".form__submit");
-            const tooltip = await formWrapper.$(".form__tooltip");
-            
-            await inputField.type(cardNum);
-            await submitButton.click();
-            
-            expect(tooltip.innerHTML).toBeUndefined();
-        },
-        timeout,
-    );
+    console.log("Launching the browser...");
+    browser = await puppeteer.launch({
+      args: process.env.CI ? ["--no-sandbox", "--disable-setuid-sandbox"] : [],
+      // headless: false,
+      // slowMo: 50,
+      // devtools: true,
+    });
+    console.log("Launched browser");
+  }, timeout);
+
+  afterAll(async () => {
+    await browser.close();
+    server.kill();
+  });
+
+  beforeEach(async () => {
+    if (page) {
+      await page.close();
+    }
+  });
+
+  test.each([
+    "253422525",
+    "000001342",
+    "ger354e",
+    " 235 sdg- 23",
+    "235678976235978523",
+    "",
+  ])(
+    "Invalid card numbers are rejected",
+    async (cardNum) => {
+      page = await browser.newPage();
+      await page.goto("http://localhost:9000", {
+        timeout: timeout,
+        waitUntil: "domcontentloaded",
+      });
+
+      const formWrapper = await page.$(
+        ".contents__wrapper .widget__wrapper .form__wrapper",
+      );
+      const form = await formWrapper.$("form");
+      const inputField = await form.$(".form__input");
+      const submitButton = await form.$(".form__submit");
+      const tooltip = await formWrapper.$(".form__tooltip");
+
+      await inputField.type(cardNum);
+      await submitButton.click();
+
+      expect(tooltip.innerHTML).not.toBe("");
+    },
+    timeout,
+  );
+
+  test.each([
+    "371449635398431",
+    "30569309025904",
+    "6011111111111117",
+    "3530111333300000",
+    "5555555555554444",
+    "4111111111111111",
+  ])(
+    "Valid card numbers pass",
+    async (cardNum) => {
+      page = await browser.newPage();
+      await page.goto("http://localhost:9000", {
+        timeout: timeout,
+        waitUntil: "domcontentloaded",
+      });
+
+      const formWrapper = await page.$(
+        ".contents__wrapper .widget__wrapper .form__wrapper",
+      );
+      const form = await formWrapper.$("form");
+      const inputField = await form.$(".form__input");
+      const submitButton = await form.$(".form__submit");
+      const tooltip = await formWrapper.$(".form__tooltip");
+
+      await inputField.type(cardNum);
+      await submitButton.click();
+
+      expect(tooltip.innerHTML).toBeUndefined();
+    },
+    timeout,
+  );
 });
--- e2e/test-server.js
+++ e2e/test-server.js
@@ -2,18 +2,20 @@ const Webpack = require("webpack");
 const WebpackDevServer = require("webpack-dev-server");
 const WebpackConfig = require("../webpack.dev.js");
 
-const server = new WebpackDevServer(WebpackConfig.devServer, Webpack(WebpackConfig));
-
 (async () => {
-  const server = new WebpackDevServer(WebpackConfig.devServer, Webpack(WebpackConfig));
+  const server = new WebpackDevServer(
+    WebpackConfig.devServer,
+    Webpack(WebpackConfig),
+  );
 
   try {
     await server.start();
 
     if (process.send) {
-      process.send('ok');
+      process.send("ok");
     }
   } catch (err) {
+    console.log(err);
     process.exit(1);
   }
 })();
